616e7d64176815ee6043e837b20cda76859e9ab6
desertsandfeelswarmatnight <sand79535@gmail.com>
2023-10-24 15:57:15 +0900

    Add files via upload

---
+++ firebaseSDK.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+
+    apiKey: "AIzaSyA0-o_Q6npys0sK9_OqwV92mqrgsWLBivM",
+
+    authDomain: "webnovel-38bad.firebaseapp.com",
+
+    projectId: "webnovel-38bad",
+
+    storageBucket: "webnovel-38bad.appspot.com",
+
+    messagingSenderId: "492198474226",
+
+    appId: "1:492198474226:web:0d093a3461b4dee232fb6b"
+};
+
+firebase.initializeApp(firebaseConfig);
